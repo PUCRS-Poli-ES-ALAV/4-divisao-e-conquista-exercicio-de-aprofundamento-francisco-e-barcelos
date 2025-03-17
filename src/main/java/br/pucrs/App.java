c95aed9f5ab33e49c283d7f2d0f18ffeffbf6aef
Francisco Freitas <136604423+fcmfreitas@users.noreply.github.com>
2025-03-17 23:40:40 +0000

    merge sort

--- src/main/java/br/pucrs/App.java
+++ src/main/java/br/pucrs/App.java
@@ -8,6 +8,22 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        
+    }
+
+    public void mergeSort(int[] a, int n){
+        if (n <= 1){
+            return;
+        }
+        if (n > 1){
+            int div = n/ 2;
+            int[] b = new int[div];
+            mergeSort(div, n/2);
+    
+            int[] c = new int[n-div];
+            mergeSort(div, n/2);
+        }
+        merge(a, b, c);
     }
 }
+
